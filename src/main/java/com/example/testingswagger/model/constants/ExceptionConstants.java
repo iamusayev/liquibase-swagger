16778402c376d3b7fcf5085e61aafcf09c304c00
iamusayev <iamusayev1337@gmail.com>
2022-03-11 01:58:51 +0400

    add ExceptionConstants class

---
+++ src/main/java/com/example/testingswagger/model/constants/ExceptionConstants.java
@@ -0,0 +1,10 @@
+package com.example.testingswagger.model.constants;
+
+
+public class ExceptionConstants {
+
+    public static final String UNEXPECTED_EXCEPTION_CODE = "UNEXPECTED_EXCEPTION";
+    public static final String UNEXPECTED_EXCEPTION_MESSAGE = "Unexpected exception occurred";
+    public static final String STUDENT_NOT_FOUND_CODE = "STUDENT_NOT_FOUND";
+    public static final String STUDENT_NOT_FOUND_MESSAGE = "Payment with id:%s not found";
+}
