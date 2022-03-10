50c53d666a1a61eb9cc33d3f82bf66e361242606
Anar <95879636+iamusayev@users.noreply.github.com>
2022-03-11 02:00:47 +0400

    Update ExceptionConstants.java

--- src/main/java/com/example/testingswagger/model/constants/ExceptionConstants.java
+++ src/main/java/com/example/testingswagger/model/constants/ExceptionConstants.java
@@ -6,5 +6,5 @@ public class ExceptionConstants {
     public static final String UNEXPECTED_EXCEPTION_CODE = "UNEXPECTED_EXCEPTION";
     public static final String UNEXPECTED_EXCEPTION_MESSAGE = "Unexpected exception occurred";
     public static final String STUDENT_NOT_FOUND_CODE = "STUDENT_NOT_FOUND";
-    public static final String STUDENT_NOT_FOUND_MESSAGE = "Payment with id:%s not found";
+    public static final String STUDENT_NOT_FOUND_MESSAGE = "Student with id:%s not found";
 }
