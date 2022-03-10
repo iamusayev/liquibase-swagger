865b9e43dbb2d24e25ff00872b4af6e211a1e971
Anar <95879636+iamusayev@users.noreply.github.com>
2022-03-11 00:11:04 +0400

    Delete StudentRepository.java

--- src/main/java/com/example/testingswagger/dao/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.testingswagger.dao.repository;
-
-import com.example.testingswagger.dao.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentEntity,Long> {
-}
