ef60098da1152478d7dca8389486112ddf50c2a6
Anar <95879636+iamusayev@users.noreply.github.com>
2022-03-11 00:11:59 +0400

    Update StudentDto.java

--- src/main/java/com/example/testingswagger/model/dto/StudentDto.java
+++ src/main/java/com/example/testingswagger/model/dto/StudentDto.java
@@ -7,17 +7,13 @@ import lombok.Data;
 
 @Data
 @Builder
-@Schema(description = "I create StudentDto class for get http method")
 public class StudentDto {
 
 
-    @Schema(description = "description for name field", name = "firstname")
     private String name;
 
-    @Schema(description = "description for surname field", name = "lastname")
     private String surname;
 
-    @Schema(hidden = true)
     private Integer age;
 
     private Long id;
